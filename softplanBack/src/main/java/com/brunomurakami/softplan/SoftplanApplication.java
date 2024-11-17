847e34106744c92ea28d607f2c51a4f6e7fca608
Bruno Murakami <murakami.bruno@gmail.com>
2024-11-17 00:15:44 -0300

    feat(cache): redis cache
    implementa cache com redis

--- softplanBack/src/main/java/com/brunomurakami/softplan/SoftplanApplication.java
+++ softplanBack/src/main/java/com/brunomurakami/softplan/SoftplanApplication.java
@@ -3,11 +3,11 @@ package com.brunomurakami.softplan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-import javax.persistence.Entity;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = "com.brunomurakami.softplan")
 @EntityScan(basePackages = "com.brunomurakami.softplan.model")
+@EnableCaching
 public class SoftplanApplication {
 
 	public static void main(String[] args) {
--- softplanBack/src/main/java/com/brunomurakami/softplan/repository/PessoaRepository.java
+++ softplanBack/src/main/java/com/brunomurakami/softplan/repository/PessoaRepository.java
@@ -2,17 +2,12 @@ package com.brunomurakami.softplan.repository;
 
 import com.brunomurakami.softplan.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa,Long> {
-
-//    @Query(value = "SELECT p.* FROM pessoa p where p.cpf = ?1", nativeQuery = true)
-//    Optional<Pessoa> findPersonByCPF(String cpf);
 
     Optional<Pessoa> findByCpf(String cpf);
 }
